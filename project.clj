973ac549f0bb2aefd2e146c761efdcf3da2c6c47
Michael Pheasant <mpheasant@gmail.com>
2013-02-19 22:28:08 +1000

    get project to work with latest ensembl

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.bioclojure/bio.ensembl "0.1.0-SNAPSHOT"
+(defproject org.clojars.intronic/bio.ensembl "0.1.0-SNAPSHOT"
   :description "Clojure API for Ensembl data access"
-  :url "http://example.com/FIXME"
+  :url "org.clojars.intronic/bio.ensembl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["jensembl" {:url "http://jensembl.sourceforge.net/m2-repo" :checksum :ignore :snapshots false }]
                  ["biojava" {:url "http://www.biojava.org/download/maven/" :checksum :ignore :snapshots false }]]
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [uk.ac.roslin/ensembl-data-access "1.13"]
-                 [uk.ac.roslin/ensembl-config "1.68"]])
+                 [uk.ac.roslin/ensembl-data-access "1.15"]
+                 [uk.ac.roslin/ensembl-config "1.70"]])
